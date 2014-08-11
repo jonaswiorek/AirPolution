cab5967e1d5635689d7a5196bea30ad531569e12
unicitet <unicitet@gmail.com>
2014-08-11 21:22:07 +0200

    update corr.R

--- corr.R
+++ corr.R
@@ -1,11 +1,20 @@
 corr <- function(directory, threshold = 0){
         ## 'directory' is a character vector of length 1 indicating
         ## the location of the CSV files
+  
+        files <- list.files(directory, full.names=TRUE)
         
         ## 'threshold' is a numeric vector of length 1 indicating the
         ## number of completely observed observations (on all
         ## variables) required to compute the correlation between
         ## nitrate and sulfate; the default is 0
         
+        for(i in 1:332) {
+          monitor <- read.csv(files[i])
+          cr <- cor(monitor[,2], monitor[,3])
+        } 
+        
         ## Return a numeric vector of correlations
+        cr
+  
 }
